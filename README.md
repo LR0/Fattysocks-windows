baf5890554be596f988a4282a7867c3a414227f2
nxyexiong <nxyexiong@gmail.com>
2018-12-30 13:57:46 +0800

    minor change

--- README.md
+++ README.md
@@ -3,6 +3,12 @@
 Unfinished!
 
 TODO:
-	1. udp
+
+	1. udp local
+
 	2. local http proxy
+
 	3. server authentication
+
+	4. udp server has cpu issue
+
--- server_startup.py
+++ server_startup.py
@@ -5,5 +5,5 @@ from server.udptunnel import UDPServer
 
 if __name__ == '__main__':
     tcp_server = TCPServer('0.0.0.0', 6666, b'thisIsAKey')
-    udp_server = UDPServer('0.0.0.0', 7778)
+    #udp_server = UDPServer('0.0.0.0', 7778)
     asyncore.loop()
