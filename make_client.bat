127ee136336e6dc3f4ae9080a23cc145402ba434
nxyexiong <nxyexiong@gmail.com>
2018-12-30 17:11:57 +0800

    modify make files

--- make_client.bat
+++ make_client.bat
@@ -1,4 +1,5 @@
 @echo off
 pyinstaller -F -i res\icon.ico -n Fattysocks -p res --windowed --onefile client_window.pyw
 xcopy /y res\*.* dist\res\*.*
+ren dist client_release
 pause
---
+++ make_server.bat
@@ -0,0 +1,9 @@
+@echo off
+md server_release
+echo f | xcopy /y /i server_startup.py server_release\server_startup.py
+echo f | xcopy /y /i utils.py server_release\utils.py
+echo f | xcopy /y /i cipher.py server_release\cipher.py
+echo f | xcopy /y /i constants.py server_release\constants.py
+echo f | xcopy /y /i users.yaml server_release\users.yaml
+xcopy /y /i server\*.* server_release\server\*.*
+pause
